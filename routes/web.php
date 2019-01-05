0bbcc1c24dbb57dafebec20d3cf9dc544b0413c1
ntjk <32801858+ntjk@users.noreply.github.com>
2019-01-04 21:33:26 -0800

    routes a test y sueltos

---
+++ routes/web.php
@@ -0,0 +1,126 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/* what the fuck am i doing? */
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('welcome', function () {
+    return view('welcome');
+});
+
+/* oh right console commands lol */
+Auth::routes();
+
+/* basic shit */
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('quienes_somos', function(){
+  return view('quienes_somos');
+});
+
+/* REAL HACKER HOURS */
+/* sucursal */
+Route::resource('sucursal','SucursalController');
+Route::post('sucursal/getOne','SucursalController@getOne');
+Route::post('sucursal/updateSelect','SucursalController@updateSelect');
+Route::get('sucursal', 'SucursalController@index');
+Route::get('sucursal-getData','SucursalController@getData')->name('sucursal_getData');
+// @eso quiere decir el metodo eso
+/* rol */
+Route::resource('rol','RolController');
+Route::post('rol/getOne','RolController@getOne');
+Route::post('rol/updateSelect','RolController@updateSelect');
+Route::get('rol','RolController@index');
+Route::get('rol-getData','RolController@getData')->name('rol_getData');
+
+/* usuario */
+Route::resource('usuario','UsuarioController');
+Route::post('usuario/getOne','UsuarioController@getOne');
+Route::post('usuario/updateSelect','UsuarioController@updateSelect');
+Route::get('usuario','UsuarioController@index');
+Route::get('usuario-getData','UsuarioController@getData')->name('usuario_getData');
+
+/* empleado */
+Route::resource('empleado','EmpleadoController');
+Route::post('empleado/getOne','EmpleadoController@getOne');
+Route::post('empleado/updateSelect','EmpleadoController@updateSelect');
+Route::post('empleado/updateSelect2','EmpleadoController@updateSelect2');
+Route::get('empleado','EmpleadoController@index');
+Route::get('empleado-getData','EmpleadoController@getData')->name('empleado_getData');
+
+/* ruta */
+Route::resource('ruta','RutaController');
+Route::post('ruta/getOne','RutaController@getOne');
+Route::post('ruta/updateSelect','RutaController@updateSelect');
+Route::get('ruta','RutaController@index');
+Route::get('ruta-getData','RutaController@getData')->name('ruta_getData');
+
+/* Flota Terrestre */
+Route::resource('transporte','FlotaController');
+Route::post('transporte/getOne','FlotaController@getOne');
+Route::post('transporte/updateSelect','FlotaController@updateSelect');
+Route::get('transporte', 'FlotaController@index');
+Route::get('transporteT-getData','FlotaController@getDataT')->name('transporteT_getData');
+
+/* Flota Maritima */
+Route::resource('transporteM','FlotaController');
+Route::post('transporteM/getOne','FlotaController@getOne');
+Route::post('transporteM/updateSelect','FlotaController@updateSelect');
+Route::get('transporteM', 'FlotaController@indexM');
+Route::get('transporteM-getData','FlotaController@getDataM')->name('transporteM_getData');
+
+/* Flota Aerea */
+Route::resource('transporteA','FlotaController');
+Route::post('transporteA/getOne','FlotaController@getOne');
+Route::post('transporteA/updateSelect','FlotaController@updateSelect');
+Route::get('transporteA', 'FlotaController@indexA');
+Route::get('transporteA-getData','FlotaController@getDataA')->name('transporteA_getData');
+
+/*Cliente */
+Route::resource('cliente','ClienteController');
+Route::post('cliente/getOne','ClienteController@getOne');
+Route::post('cliente/updateSelect','ClienteController@updateSelect');
+Route::post('cliente/updateSelect2','ClienteController@updateSelect2');
+Route::get('cliente', 'ClienteController@index');
+Route::get('cliente-getData','ClienteController@getData')->name('cliente_getData');
+
+/* Envio */
+Route::resource('envio','EnvioController');
+Route::post('envio/getOne','EnvioController@getOne');
+Route::post('envio/updateSelect','EnvioController@updateSelect');
+Route::get('envio', 'EnvioController@index');
+Route::get('envio-getData','EnvioController@getData')->name('envio_getData');
+
+/* ruta */
+Route::resource('floru','FloruController');
+Route::post('floru/getOne','FloruController@getOne');
+Route::post('floru/updateSelect','FloruController@updateSelect');
+Route::get('floru','FloruController@index');
+//Route::get('floru-getData','FloruController@getData')->name('floru_getData');
+
+
+Route::get("/aja",function(){
+$resul=DB::select("select * from sucursal where su_clave = ?", [12]);
+	foreach($resul as $resu){
+		return $resu->su_nombre;
+	}
+});
+/*Route::get("/aje",function(){
+$resul=DB::select("select en_tipo, en_precio, en_peso, en_descripcion, en_altura, en_anchura, en_profundidad, en_fecha_envio, en_fecha_entrega_estimada, sucursalo.su_nombre, cli_cedula, des_cedula, fk_flota_ruta_1, sucursald.su_nombre 
+from sucursal as sucursalo, sucursal as sucursald, envio, cliente, destinatario where 
+sucursalo.su_clave=fk_sucursal_origen and sucursald.su_clave=fk_sucursal_destino and cli_clave=fk_cliente and des_clave=fk_destinatario");
+	foreach($resul as $resu){
+		return $resu->su_nombre, $res;
+	}
+});
+
+//select devuelve array t recorrer elemento por ele para leerlo
---
+++ storage/framework/views/35456dac70d4655e27efaf76edd7fa7031c8ada6.php
@@ -0,0 +1,218 @@
+<!doctype html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Lato', sans-serif;
+                font-weight: 200;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                width: 100vw;
+                background-color: #ffc002;
+                text-align: center;
+                height:200px;
+            }
+
+            .title {
+                font-size: 46px;
+                text-decoration: underline;
+            }
+
+            .links > a {
+                width:100vw;
+                color: #ffffff;
+                padding: 0 10px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+                float:none;
+            }
+
+            .m-b-md {
+              margin-top: 20px;
+                text-align: center;
+                margin-left: 30px;
+            }
+            .logo-container{
+              display: inline;
+              float: left;
+              width: 30vw;
+            }
+            .logo{
+                height:200px;
+            }
+
+            .motto-container{
+              width:70vw;
+              float:right;
+            }
+
+            .motto{
+              color:#ffffff;
+              font-family: 'Abril Fatface', cursive;
+                  padding-top: 45px;
+                  font-size: 52px;
+                  text-decoration: bold;
+                  text-align: center;
+            }
+
+            .nav{
+              text-align: center;
+              width:100vw;
+              height: 30px;
+              background-color: #000000;
+            }
+            .main{
+              padding-top: 50px;
+              background-color: #404040;
+              height: auto;
+            }
+            .description{
+              display: block;
+              padding-bottom: 1px;
+              margin-top:20px;
+              margin-bottom:50px;
+              margin-left: 20vw;
+              margin-right: 20vw;
+              color: #ffffff;
+              text-align: center;
+            }
+          .banner{
+            height:350px;
+          }
+          .line{
+            background-color: #000000;
+            height: 3px;
+            margin-left: 2vw;
+            width: 95vw;
+          }
+          .footer{
+            text-align: center;
+            margin-top: 50px;
+          }
+          .half-container{
+            margin: 0px;
+            width:80vw;
+            padding-left: 10vw;
+            padding-right: 10vw;
+            overflow: hidden;
+          }
+          .half{
+            margin-left: 0;
+            margin-right: 0;
+            border: 20vw;
+            float:left;
+            max-width: 40vw;
+            width: auto;
+          }
+          .half h1{
+            color: #45a5ed;
+          }
+        </style>
+    </head>
+    <body>
+      <!--  <div class="flex-center position-ref full-height">
+            <?php if(Route::has('login')): ?>
+                <div class="top-right links">
+                    <?php if(auth()->guard()->check()): ?>
+                        <a href="<?php echo e(url('/home')); ?>">Home</a>
+                    <?php else: ?>
+                        <a href="<?php echo e(route('login')); ?>">Login</a>
+
+                        <?php if(Route::has('register')): ?>
+                            <a href="<?php echo e(route('register')); ?>">Register</a>
+                        <?php endif; ?>
+                    <?php endif; ?>
+                </div>
+            <?php endif; ?> -->
+
+            <div class="content">
+              <div class="logo-container">
+              <div class="logo">
+                <a href="welcome"><img src=<?php echo e(asset('img/logo.png')); ?> class="logo"></img></a>
+              </div>
+            </div>
+            <div class="motto-container">
+              <div class="motto">
+                  Mayor empresa de envios en venezuela.
+              </div>
+            </div>
+            </div>
+              <div class="nav">
+                <div class="links">
+                    <a href="quienes_somos">¿Quienes Somos?</a>
+                    <a href="usuario">Usuarios</a>
+                    <a href="rol">Roles</a>
+                    <a href="sucursal">Sucursales</a>
+                    <a href="transporte">Transportes T</a>
+                    <a href="transporteM">Transportes M</a>
+                    <a href="transporteA">Transportes A</a>
+                    <a href="cliente">Clientes</a>
+                    <a href="empleado">Empleados</a>
+                    <a href="ruta">Rutas</a>
+                    <a href="envio">Envios</a>
+                </div>
+              </div>
+              <div class="main">
+                <div class="description">
+                  En 1986 la UCAB extiende sus negocios con la apertura de la primera compañía de entregas dentro de Venezuela LogUCAB. Hoy en día hacemos traslados de paquetes a toda América Latina y Europa (España, Portugal y Francia en miras de seguir expandiendo). Estamos comprometidos con la sostenibilidad, la satisfacción del cliente, la protección del medio ambiente, la seguridad de las personas y de la información
+                </div>
+                <div class="half-container">
+                  <div class="description half">
+                  <h1>Misión</h1> <br> Aportar soluciones para hacer ganar tiempo uniendo personas y distancias, comprometidos con un mundo más  sostenible.
+                </div>
+                    <div class="description half">
+                      <h1>Visión</h1> <br> Ser percibida como la marca líder preferida por ofrecer las soluciones más innovadoras y de valor añadido en el transporte y la logística.
+                    </div>
+                  </div>
+
+                </div>
+              <div class="line-container">
+                <div class="line">
+                </div>
+              </div>
+              <div class="footer">
+                Envíos UCAB, Rif: J-00274758-7, LogUCAB Venezuela todos los derechos reservados<br>
+Diseñado y desarrollado por SSR Lerana C.A.
+
+              </div>
+        </div>
+    </body>
+</html>
