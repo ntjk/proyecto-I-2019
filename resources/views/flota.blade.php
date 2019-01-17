15e1cd44fc730179cee8f15c83acc16f9716ec86
Leo C <leole666@gmail.com>
2019-01-17 05:29:19 -0400

    FLOTA

--- resources/views/flota.blade.php
+++ resources/views/flota.blade.php
@@ -12,7 +12,7 @@
         <meta name="csrf-token" content="{!! csrf_token() !!}" />
         <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
         <script type="text/javascript" src="{{ asset('js/dropdown.js') }}"></script>
-        <title>Aéreo - LogUCAB</title>
+        <title>Terrestre - LogUCAB</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -23,7 +23,7 @@
             @include('header')
             <div class="container">
             <br/>
-            <h1 class="text-center">Transportes Aéreos</h1>
+            <h1 class="text-center">Transporte Terrestre</h1>
             <br/>
             <button type="button" id="add_button" data-toggle="modal" data-target="#userModal" class="btn btn-info btn-lg">Add</button>
             <table class="table table-bordered" id="users-table">
@@ -47,7 +47,7 @@
                     </tr>
                 </thead>
                 <tbody>
-                  @foreach ($flotas as $floa)
+                  @foreach ($flotas as $flote)
                   <tr>
                     <td>{{$flote->flo_clave}}</td>
                     <td>{{$flote->flo_subtipo}}</td>
@@ -79,81 +79,60 @@
    <div class="modal-content">
     <div class="modal-header">
      <button type="button" class="close" data-dismiss="modal">&times;</button>
-     <h4 class="modal-title">Añadir Flota Aérea</h4>
+     <h4 class="modal-title">Añadir Flota Terrestre</h4>
     </div>
     <div class="modal-body">
-     <label>Subtipo</label>
-     <select class="form-control" name="flo_subtipo" id="flo_subtipo">
-       <option value="aerea" selected>aerea</option>
-     </select>
-     <br />
-     <label>Tipo</label>
-     <input type="text" name="flo_tipo" id="flo_tipo" class="form-control" />
-     <br />
-     <label>Peso</label>
-     <input type="number" step="0.01" name="flo_peso" id="flo_peso" class="form-control" />
-     <br />
-     <label>Placa</label>
-     <input type="text" name="flo_placa" id="flo_placa" class="form-control" />
-     <br />
-     <label>Descripción</label>
-     <input type="text" name="flo_descripcion" id="flo_descripcion" class="form-control" />
-     <br />
-     <label>Combustible por hora</label>
-     <input type="number" step="0.01" name="flo_combustible_por_hora" id="flo_combustible_por_hora" class="form-control" />
-     <br />
-     <label>Serial de carroceria</label>
-     <input type="text" name="flo_serial_carroceria" id="flo_serial_carroceria" class="form-control" />
-     <br />
-     <label>Capacidad de carga</label>
-     <input type="number" step="0.01" name="flo_capacidad_carga" id="flo_capacidad_carga" class="form-control" />
-     <br />
-     <label>Modelo</label>
-     <select class="form-control" name="fk_modelo" id="fk_modelo">
+    <label>Subtipo</label>
+        <select class="form-control" name="flo_subtipo" id="flo_subtipo">
+        <option value="terrestre" selected>terrestre</option>
+        </select>
+        <br />
+        <label>Tipo</label>
+        <input type="text" name="flo_tipo" id="flo_tipo" class="form-control" />
+        <br />
+        <label>Peso</label>
+        <input type="number" step="0.01" name="flo_peso" id="flo_peso" class="form-control" />
+        <br />
+        <label>Placa</label>
+        <input type="text" name="flo_placa" id="flo_placa" class="form-control" />
+        <br />
+        <label>Descripción</label>
+        <input type="text" name="flo_descripcion" id="flo_descripcion" class="form-control" />
+        <br />
+        <label>Combustible por hora</label>
+        <input type="number" step="0.01" name="flo_combustible_por_hora" id="flo_combustible_por_hora" class="form-control" />
+        <br />
+        <label>Serial de carroceria</label>
+        <input type="text" name="flo_serial_carroceria" id="flo_serial_carroceria" class="form-control" />
+        <br />
+        <label>Capacidad de carga</label>
+        <input type="number" step="0.01" name="flo_capacidad_carga" id="flo_capacidad_carga" class="form-control" />
+        <br />
+        <label>Modelo</label>
+        <select class="form-control" name="fk_modelo" id="fk_modelo">
         @foreach($modelos as $modelo)
         <option value="{{$modelo->mod_clave}}">{{$modelo->mod_nombre}}</option>
         @endforeach
-      </select>
-     <label>Sucursal Base</label>
-     <select class="form-control" name="fk_sucursal" id="fk_sucursal">
+        </select>
+        <label>Sucursal Base</label>
+        <select class="form-control" name="fk_sucursal" id="fk_sucursal">
         @foreach($sucursales as $sucursal)
         <option value="{{$sucursal->su_clave}}">{{$sucursal->su_nombre}}</option>
         @endforeach
-      </select>
-     <br />
-     <label>Año</label>
-     <input type="text" name="flo_año" id="flo_año" class="form-control" />
-     <br />
-     <label>Longitud</label>
-     <input type="number" step="0.01" name="flo_a_longitud" id="flo_a_longitud" class="form-control" />
-     <br />
-     <label>Envergadura</label>
-     <input type="number" step="0.01" name="flo_a_envergadura" id="flo_a_envergadura" class="form-control" />
-     <br />
-     <label>Area</label>
-     <input type="number" step="0.01" name="flo_a_area" id="flo_a_area" class="form-control" />
-     <br />
-     <label>Altura</label>
-     <input type="number" step="0.01" name="flo_a_altura" id="flo_a_altura" class="form-control" />
-     <br />
-     <label>Ancho Cabina Interna</label>
-     <input type="number" step="0.01" name="flo_a_ancho_cabina_interna" id="flo_a_ancho_cabina_interna" class="form-control" />
-     <br />
-     <label>Diametro de Fuselaje</label>
-     <input type="number" step="0.01" name="flo_a_diametro_fuselaje" id="flo_a_diametro_fuselaje" class="form-control" />
-     <br />
-     <label>Peso Vacio</label>
-     <input type="number" step="0.01" name="flo_a_peso_vacio" id="flo_a_peso_vacio" class="form-control" />
-     <br />
-     <label>Peso Maximo Despegue</label>
-     <input type="number" step="0.01" name="flo_a_peso_maximo_despegue" id="flo_a_peso_maximo_despegue" class="form-control" />
-     <br />
-     <label>Carrera de Despegue</label>
-     <input type="number" step="0.01" name="flo_a_carrera_de_despegue" id="flo_a_carrera_de_despegue" class="form-control" />
-     <br />
-     <label>Velocidad de Despegue</label>
-     <input type="number" step="0.01" name="flo_a_velocidad_maxima" id="flo_a_velocidad_maxima" class="form-control" />
-     <br />
+        </select>
+        <br />
+        <label>Año</label>
+        <input type="text" name="flo_año" id="flo_año" class="form-control" />
+        <br />
+        <label>Es Nacional?</label>
+        <select class="form-control" name="flo_te_nacional" id="flo_te_nacional">
+        <option value="true">Si</option>
+        <option value="false">No</option>
+        </select>
+        <br />
+        <label>Serial del Motor</label>
+        <input type="text" name="flo_te_serial_motor" id="flo_te_serial_motor" class="form-control" />
+        <br />
     </div>
     <div class="modal-footer">
      <input type="hidden" name="flo_clave" id="flo_clave" />
@@ -208,21 +187,13 @@
             var fk_modelo = $('#fk_modelo').val();
             var fk_sucursal = $('#fk_sucursal').val();
             var flo_año = $('#flo_año').val();
-            var flo_a_longitud = $('#flo_a_longitud').val();
-            var flo_a_envergadura = $('#flo_a_envergadura').val();
-            var flo_a_area = $('#flo_a_area').val();
-            var flo_a_altura = $('#flo_a_altura').val();
-            var flo_a_ancho_cabina_interna = $('#flo_a_ancho_cabina_interna').val();
-            var flo_a_diametro_fuselaje = $('#flo_a_diametro_fuselaje').val();
-            var flo_a_peso_vacio = $('#flo_a_peso_vacio').val();
-            var flo_a_peso_maximo_despegue = $('#flo_a_peso_maximo_despegue').val();
-            var flo_a_carrera_de_despegue = $('#flo_a_carrera_de_despegue').val();
-            var flo_a_velocidad_maxima = $('#flo_a_velocidad_maxima').val();
+            var flo_te_nacional = $('#flo_te_nacional').val();
+            var flo_te_serial_motor = $('#flo_te_serial_motor').val();
             if(flo_subtipo != '' && flo_tipo != '')
             {
               $.ajax({
                 headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                url:"transporteA",
+                url:"transporte",
                 method:'POST',
                 data: new FormData(this),
                 contentType:false,
@@ -240,16 +211,21 @@
               alert("Both Fields are Required");
             }
           });
+
           $(document).on('click', '.update', function(){
             var flo_clave = $(this).attr("id");
             $.ajax({
               headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-              url:"transporteA/getOne",
+              url:"transporte/getOne",
               method:"POST",
               data:{flo_clave:flo_clave},
               dataType:"json",
               success:function(data){
                 $('#userModal').modal('show');
+                $('.modal-title').text("Edit Flota");
+                $('#userModal').modal('show');
+                $('.modal-title').text("Edit Flota");
+                $('#flo_clave').val(flo_clave);
                 $('#flo_subtipo').val(data.flo_subtipo);
                 $('#flo_tipo').val(data.flo_tipo);
                 $('#flo_peso').val(data.flo_peso);
@@ -258,32 +234,23 @@
                 $('#flo_combustible_por_hora').val(data.flo_combustible_por_hora);
                 $('#flo_serial_carroceria').val(data.flo_serial_carroceria);
                 $('#flo_capacidad_carga').val(data.flo_capacidad_carga);
-                $('#flo_año').val(data.flo_año);
-                $('#flo_a_longitud').val(data.flo_a_longitud);
-                $('#flo_a_envergadura').val(data.flo_a_envergadura);
-                $('#flo_a_area').val(data.flo_a_area);
-                $('#flo_a_altura').val(data.flo_a_altura);
-                $('#flo_a_ancho_cabina_interna').val(data.flo_a_ancho_cabina_interna);
-                $('#flo_a_diametro_fuselaje').val(data.flo_a_diametro_fuselaje);
-                $('#flo_a_peso_vacio').val(data.flo_a_peso_vacio);
-                $('#flo_a_peso_maximo_despegue').val(data.flo_a_peso_maximo_despegue);
-                $('#flo_a_carrera_de_despegue').val(data.flo_a_carrera_de_despegue);
-                $('#flo_a_velocidad_maxima').val(data.flo_a_velocidad_maxima);
-                $('.modal-title').text("Edit Flota");
-                $('#flo_clave').val(flo_clave);
+                $('#flo_te_nacional').val(data.flo_te_nacional);
+                $('#flo_te_serial_motor').val(data.flo_te_serial_motor);
                 $('#fk_modelo').val(fk_modelo);
                 $('#fk_sucursal').val(fk_sucursal);
+                $('#flo_año').val(data.flo_año);
                 $('#action').val("Edit");
                 $('#operation').val("Edit");
               }
             })
           });
+
           $(document).on('click','.delete',function(){
             var flo_clave = $(this).attr("id");
             if(confirm("¿Estás seguro de que quieres borrar esta información?")){
               $.ajax({
                 headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                url:"transporteA/"+flo_clave,
+                url:"transporte/"+flo_clave,
                 type:"DELETE",
                 data:{flo_clave:flo_clave},
                 success:function(data){
@@ -302,7 +269,6 @@
             var url = "{{url('/historicoF')}}" + flo_clave;
             window.location.href = url;
           });
-
 
         });
         </script>
